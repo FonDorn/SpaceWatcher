da980655668129809c3b27cbc7ed703e49649cc6
Mike Nedosekin <crespo8800@gmail.com>
2017-01-23 20:20:43 +0200

    Change background

--- index.vr.js
+++ index.vr.js
@@ -12,19 +12,7 @@ class SpaceWatcher extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor:'blue',
-            padding: 0.02,
-            textAlign:'center',
-            textAlignVertical:'center',
-            fontSize: 0.8,
-            layoutOrigin: [0.5, 0.5],
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <Pano source={asset('space-bg.png')}/>
       </View>
     );
   }
